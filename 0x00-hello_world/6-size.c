eff83d2c69f597b54b5bc74de080bacd8d326713
kennyChukwuebuka <kenkinado@gmail>
2023-06-15 12:44:11 -0700

    Script that prints size of files

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - This program prints computer sizes
+ * Return: 0
+ */
+int main(void)
+{
+	printf("Size of a char: %c byte(s)\n", '1');
+	printf("Size of an int: %c byte(s)\n", '4');
+	printf("Size of a long int: %c byte(s)\n", '8');
+	printf("Size of a long long int: %c byte(s)\n", '8');
+	printf("Size of a float: %c byte(s)\n", '4');
+	return (0);
+}
