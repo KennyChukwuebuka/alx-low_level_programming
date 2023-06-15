dbaa731ef398212a66e816f293c0115e0914229a
kennyChukwuebuka <kenkinado@gmail>
2023-06-15 13:05:47 -0700

    Updated script for file print

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,16 @@
  */
 int main(void)
 {
-	printf("Size of a char: %c byte(s)\n", '1');
-	printf("Size of an int: %c byte(s)\n", '4');
-	printf("Size of a long int: %c byte(s)\n", '4');
-	printf("Size of a long long int: %c byte(s)\n", '8');
-	printf("Size of a float: %c byte(s)\n", '4');
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
