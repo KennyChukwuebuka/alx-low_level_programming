557813b6f5fbf33ce88e4a60c8a59914b5f2467b
kennyChukwuebuka <kenkinado@gmail>
2023-06-15 12:46:52 -0700

    Updated the script that prints file sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,7 @@ int main(void)
 {
 	printf("Size of a char: %c byte(s)\n", '1');
 	printf("Size of an int: %c byte(s)\n", '4');
-	printf("Size of a long int: %c byte(s)\n", '8');
+	printf("Size of a long int: %c byte(s)\n", '4');
 	printf("Size of a long long int: %c byte(s)\n", '8');
 	printf("Size of a float: %c byte(s)\n", '4');
 	return (0);
