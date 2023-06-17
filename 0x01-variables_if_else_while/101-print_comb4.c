7c2ac7e20803d0969910a98e5115384ac7f8d314
kennyChukwuebuka <kenkinado@gmail>
2023-06-16 19:21:54 -0700

    Updated the last combination file script

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,37 +1,32 @@
 #include <stdio.h>
 /**
- * main - Print combination of digits
+ * main - print combination of three digits
  * Return: 0
  */
 int main(void)
-{       
-        int m, n, o;
-        int putcharCount = 0;
-        
-        for (m = 48; m < 58; m++)
-        {       
-                for (n = m + 1; n < 58; n++)
-                {       
-                        for (o = n + 1; o < 58; o++)
-                        {       
-                                putchar(m);
-                                putchar(n);
-                                putchar(o);
-                                putcharCount +=3;
-                                if (putcharCount < 6)
+{
+	int m, n, o;
+
+	for (m = 48; m < 58; m++)
+	{
+		for (n = 49; n < 58; n++)
+		{
+			for (o = 50; o < 58; o++)
+			{
+				if (o > n && n > m)
 				{
-					putchar(',');
-					putchar(' ');
-					putcharCount += 2;
-				}
-				else
-				{
-					putchar('\n');
-					return 0;
+					putchar(m);
+					putchar(n);
+					putchar(o);
+					if (m != 55 || n != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
 			}
 		}
 	}
 	putchar('\n');
-	return 0;
+	return (0);
 }
