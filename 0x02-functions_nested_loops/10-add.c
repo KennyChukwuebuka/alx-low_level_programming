7b288cfd2aa72d72255f1c408f8be02389bb41a2
kennyChukwuebuka <kenkinado@gmail>
2023-06-20 12:56:50 -0700

    This files will add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - This will add two int values
+ * @a: int to be added b
+ * @b: int to be added again to a
+ * Return: 0
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,6 +4,7 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
+int add(int, int);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 void jack_bauer(void);
