8e3113cb8d4eca3f964e14859d37ad6c1786f86e
kennyChukwuebuka <kenkinado@gmail>
2023-06-20 14:56:31 -0700

    This file prints natural numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stio.h>
+/**
+ * main - List all the natural numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int i, sum = 0;
+	
+	for (i = 0; i <= 1024; i++)
+	{
+		if ((i % 3) = 0 || (i % 5) = 0)
+			sum += 1;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
