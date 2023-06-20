e9c310aef44153fdcd74462a94364ddcc28d863f
kennyChukwuebuka <kenkinado@gmail>
2023-06-20 15:06:38 -0700

    Updated

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,13 +6,12 @@
 int main(void)
 {
 	int i, sum = 0;
-	
+
 	for (i = 0; i <= 1024; i++)
 	{
-		if ((i % 3) = 0 || (i % 5) = 0)
+		if ((i % 3) == 0 || (i % 5) == 0)
 			sum += 1;
 	}
-
 	printf("%d\n", sum);
 
 	return (0);
