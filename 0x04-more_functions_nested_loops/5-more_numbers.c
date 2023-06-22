6287ad90c7a0d8bc0f2d0ab69239727a52dc8787
kennyChukwuebuka <kenkinado@gmail>
2023-06-22 13:24:21 -0700

    This script prints lines

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,26 +1,2 @@
-#include "main.h"
-/**
- * more_numbers - This script prints numbers more times
- * Return: 0
- */
-void more_numbers(void)
-{
-	int k, l;
+#include "main.h":wq
 
-	for (k = 0; k < 10; k++)
-	{
-		for (l = 0; l <= 14; l++)
-		{
-			if (l < 10)
-			{
-				_putchar(l + '0');
-			}
-			else
-			{
-				_putchar('1');
-				_putchar(l % 10 + '0');
-			}
-		}
-		_putchar('\n');
-	}
-}
