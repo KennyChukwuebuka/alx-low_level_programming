1c15489ba6deece75c46400d29ea8e01a5bbec2b
kennyChukwuebuka <kenkinado@gmail>
2023-06-26 12:30:58 -0700

    This script prints half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - This script prints half of a string
+ * @str: String val to print
+ */
+
+void puts_half(char *str)
+{
+	int x, y, i;
+
+	x = strlen(str);
+
+	if (x % 2 == 1)
+		y = x / 2 + 1;
+	else
+		y = x / 2;
+
+	for (i = y; i < x; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
+
