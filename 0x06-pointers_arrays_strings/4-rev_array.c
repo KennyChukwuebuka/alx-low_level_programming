abd7561a95c3e7f5c0eadf83ca5399f5b45625c0
kennyChukwuebuka <kenkinado@gmail>
2023-06-29 09:19:16 -0700

    This script returns lower character to upper

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * reverse_array - This reverse an array of integers
+ * @a: initial desc
+ * @n: num
+ * Return: Integers
+ */
+
+void reverse_array(int *a, int n)
+{
+
