a1f21c2bd3840c137a8495eee2400e0f7060b4db
kennyChukwuebuka <kenkinado@gmail>
2023-06-29 14:45:31 -0700

    This scrit prints a buffer

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * infinite_add - func that adds two numbers
+ * @n1: params one
+ * @n2: params two
+ * @r: buffer to use to store func
+ * @size_r: buffer size
+ * Return: A pointer
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+
+
