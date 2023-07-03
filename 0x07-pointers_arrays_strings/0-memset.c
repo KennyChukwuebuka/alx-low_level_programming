37c7c51d96be55aa4a5baba84730be301c889f9c
kennyChukwuebuka <kenkinado@gmail>
2023-07-03 07:21:44 -0700

    Updated

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -12,6 +12,14 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
+	/*while (i < n)*/
+	/*{*/
+		/*s[i] = b;*/
+		/*i++;*/
+	/*}*/
+	/*return (s);*/
+/*}*/
+
 	for (i = 0; i < n; i++)
 		s[i] = b;
 	return (s);
