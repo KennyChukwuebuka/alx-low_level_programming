77c486de9c6d8de56001a3c895d7b39fd71a750b
kennyChukwuebuka <kenkinado@gmail>
2023-07-05 13:23:28 -0700

    function that compares two strings and returns 1 if the strings can be considered identical, otherwise return 0

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -12,6 +12,7 @@ int real_prime(int n, int i);
 int is_palindrome(char *s);
 int natural_check(char *s, int i, int len);
 int _strlen_recursion(char *s);
+int wildcmp(char *s1, char *s2);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 
