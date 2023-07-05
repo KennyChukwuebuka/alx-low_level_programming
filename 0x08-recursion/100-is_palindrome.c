f41b211a5d495edd5299c07b54d2bad8a5417678
kennyChukwuebuka <kenkinado@gmail>
2023-07-05 13:00:02 -0700

    Functuion that return 1 if string is palindrome or 0 if not

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+int natural_check(char *s, int i, int len);
+int _strlen_recursion(char *s);
+
+/**
+ * is_palindrome - func that ret 1 if string is palindrom
+ * @s: string to rev
+ * Return 1 if palindrome or 0 if not
+ */
+
+int is_palindrome(char *s)
+{
+	if (*s == 0)
+		return (1);
+	return (natural_check(s, 0, _strlen_recursion(s)));
+}
+
+/**
+ * _strlen_recursion - ret lenght of string
+ * @s: string to calc
+ * Return: length of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * natural_check - check char recursively
+ * @s: checks string
+ * @i: iterator
+ * @len: length of str
+ * Return: 1 if palindrome or 0
+ */
+int natural_check(char *s, int i, int len)
+{
+	if (*(s + i) != *(s + len - 1))
+		return (0);
+	if (i >= len)
+		return (1);
+	return (natural_check(s, i + 1, len - 1));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,6 +9,9 @@ int _sqrt_recursion(int n);
 int _sqrt_recursion_natural(int n, int s, int y);
 int is_prime_number(int n);
 int real_prime(int n, int i);
+int is_palindrome(char *s);
+int natural_check(char *s, int i, int len);
+int _strlen_recursion(char *s);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 
