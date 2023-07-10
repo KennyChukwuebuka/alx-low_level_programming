b5ff279aa15b786d00ef273408224a1cea94ee46
kennyChukwuebuka <kenkinado@gmail>
2023-07-10 15:17:22 -0700

    function that frees a 2 dimensional grid previously created by your alloc_grid function

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * argstostr - Function that concat argstostr
+ * @ac: params 1
+ * @av: params 2
+ * Return: String
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int i;
+
+	for (i = 0; i < 
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -4,7 +4,9 @@
 int _putchar(char c);
 int **alloc_grid(int width, int height);
 char *create_array(unsigned int size, char c);
+char *argstostr(int ac, char **av);
 char *str_concat(char *s1, char *s2);
 char *_strdup(char *str);
+void free_grid(int **grid, int height);
 
 #endif
