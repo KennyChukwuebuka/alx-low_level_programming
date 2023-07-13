b9800d04dae4961e1392281e842b32f27b120090
kennyChukwuebuka <kenkinado@gmail>
2023-07-13 14:52:31 -0700

    fix

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -33,7 +33,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (j = 0; j < len2; j++)
 		nStr[i + j] = s2[j];
 
-	nStr[len1 + len2] = '\0';
+	nStr[len1 + len2 + 1] = '\0';
 
 	return (nStr);
 }
