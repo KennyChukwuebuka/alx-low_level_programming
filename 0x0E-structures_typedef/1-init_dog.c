05e36444502be68fae20772bc48c989a7f4a0193
kennyChukwuebuka <kenkinado@gmail>
2023-07-17 09:50:30 -0700

    A type struct of dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * init_dog - structure of dog
+ @d: pointer to dog
+ * @name: name of dog
+ * @age of dog
+ * @owner: owner of dog
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef STRTYP
+#define STRTYP
+
+/**
+ * struct dog - structure of dog
+ * @name: name of dog
+ * @age of dog
+ * @owner: owner of dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif /*STRTYP*/
