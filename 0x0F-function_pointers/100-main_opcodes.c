0fe67e8f5f9a0e613eeba01371a8b9cdb3d9143c
kennyChukwuebuka <kenkinado@gmail>
2023-07-19 11:46:15 -0700

    Hot fix

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -13,29 +13,31 @@ int main(int argc, char *argv[])
 	int num_byte, i;
 	char *array;
 
-	if (argc != 2)
+	array = (char *)main;
+
+	if (argc != 2) /*check if number of argument is not correct*/
 	{
 		printf("Error\n");
 		exit(1);
 	}
 
-	num_byte = atoi(argv[1]);
+	num_byte = atoi(argv[1]); /*convert the argument to int*/
 
-	if (num_byte < 0)
+	if (num_byte < 0) /*check if the number of byte is negative*/
 	{
 		printf("Error\n");
 		exit(2);
 	}
-	array = (char *)main;
+	/*array = (char *)main;*/
 
 	for (i = 0; i < num_byte; i++)
 	{
 		if (i == num_byte - 1)
 		{
-			printf("%02hhx\n", array[i]);
+			printf("%02hhx\n", array[i]); /*print the bytes in hex*/
 			break;
 		}
-		printf("%02hhx ", array[i]);
+		printf("%02hhx ", array[i]); /*print the bytes in hex*/
 	}
 	return (0);
 }
