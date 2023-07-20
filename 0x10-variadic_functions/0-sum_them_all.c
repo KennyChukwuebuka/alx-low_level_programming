cb606d628353938fc8f09bec2903e6f17f9faf9b
kennyChukwuebuka <kenkinado@gmail>
2023-07-20 09:04:16 -0700

    Requirment fix

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -17,9 +17,9 @@ int sum_them_all(const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		int arg = va_arg(args, int);
+
 		sum += arg;
 	}
-		/*sum += va_arg(args, int);*/
 	va_end(args);
 
 	return (sum);
