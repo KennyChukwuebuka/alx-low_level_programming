700ee52feddaa854778aba245cb9415d7b90c294
kennyChukwuebuka <kenkinado@gmail>
2023-07-20 09:13:23 -0700

    hot fix

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -15,7 +15,11 @@ int sum_them_all(const unsigned int n, ...)
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
-		sum += va_arg(args, int);
+	{
+		int arg = va_arg(args, int);
+
+		sum += arg;
+	}
 
 	va_end(args);
 
