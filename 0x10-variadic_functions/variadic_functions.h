7c39bb1737d161f8120546f76ea37dd82daf9d1b
kennyChukwuebuka <kenkinado@gmail>
2023-07-20 09:21:32 -0700

    hot fix in header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,8 @@
 #ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC_FUCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+#include <stdarg.h>
+#include <stdio.h>
 
 int sum_them_all(const unsigned int n, ...);
 int _putchar(char c);
