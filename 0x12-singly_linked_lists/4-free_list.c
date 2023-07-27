47940db692527c2d07c94883b8f6ecd51b0ced4a
kennyChukwuebuka <kenkinado@gmail>
2023-07-27 08:33:53 -0700

    Function that free

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list - function that frees
+ * @head: param
+ * 
+ * Return: nothing
+ */
+
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
+	}
+}
