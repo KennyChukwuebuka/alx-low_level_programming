3f68cbd2dcbfb33be74c09b7e40237e773a6086d
kennyChukwuebuka <kenkinado@gmail>
2023-07-27 09:25:53 -0700

    function that prints a string before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * hare - print a function before the main is  executed
+ */
+
+void hare(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
