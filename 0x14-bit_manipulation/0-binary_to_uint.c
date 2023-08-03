157835473b9e28e7362e80650af93e09c999eb5c
kennyChukwuebuka <kenkinado@gmail>
2023-08-03 16:23:45 +0100

    Function that convert binary to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,12 +1,25 @@
 #include "main.h"
 /**
- * binary_to_unit - Function that convert binary to unit
+ * binary_to_uint - Function that convert binary to unit
  * @b: Params
- * 
- * Returns: converted number OR 0
+ *
+ * Return: converted number OR 0
  */
 
 unsigned int binary_to_uint(const char *b)
 {
+            int i;
+            unsigned int val = 0;
 
+        if (!b)
+            return (0);
+
+        for (i = 0; b[i]; i++)
+        {
+                if (b[i] < '0' || b[i] > '1')
+                         return (0);
+                        val = 2 * val + (b[i] - '0');
+    }
+
+    return (val);
 }
