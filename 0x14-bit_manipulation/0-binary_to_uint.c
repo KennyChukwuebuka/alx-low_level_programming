49d8fc7af52baa690584d9d0aab3384867142380
kennyChukwuebuka <kenkinado@gmail>
2023-08-03 14:35:37 +0100

    Commited file

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,9 @@
 #include "main.h"
 /**
+ * binary_to_unit - Function that convert binary to unit
+ * @b: Params
+ * 
+ * Returns: converted number OR 0
  */
 
 unsigned int binary_to_uint(const char *b)
