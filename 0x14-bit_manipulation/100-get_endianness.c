4bd5445667599b63a4ba971df41a0164f180c1c7
kennyChukwuebuka <kenkinado@gmail>
2023-08-03 13:42:32 -0700

    Crack me

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -8,8 +8,17 @@
 
 int get_endianness(void)
 {
-	unsigned int index = 0;
+	unsigned int index = 1;
 	char *n = (char *) &index;
 
 	return (*n);
+	/*unioin*/
+	/*{*/
+		/*unsigned int i;*/
+		/*unsigned char c[sizeof(unsigned int)];*/
+	/*}n;*/
+
+	/*n.i = 1;*/
+
+	/*return ((n.i[0] == 1) ? 1 : 0);*/
 }
