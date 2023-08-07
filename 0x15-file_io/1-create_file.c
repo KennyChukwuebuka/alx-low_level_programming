e024ffbc226ad167d8ef9d97da52c162eff4a88e
kennyChukwuebuka <kenkinado@gmail>
2023-08-07 13:36:12 -0700

    Hotfix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,30 +10,23 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int file_desc, wr, i;
+        int file_desc, wr, i = 0;
 
-	if (filename == NULL)
-		return (0);
+        if (filename == NULL)
+                return (0);
 
-	if (text_content != NULL)
-	{
-		for (i = 0; text_content[i]; i++)
-			;
-	}
-	file_desc = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	
-	if (file_desc == -1)
-		return  (-1);
+        if (text_content != NULL)
+        {
+                for (; text_content[i];)
+                        i++;
+        }
+        file_desc = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
-	wr = write(file_desc, text_content, i);
+        wr = write(file_desc, text_content, i);
 
-	if (wr == -1)
-	{
-		close(file_desc);
-		return (-1);
-	}
+        if (file_desc == -1 || wr == -1)
+                return (-1);
 
-	close(file_desc);
-	return (1);
-}
-
+        close(file_desc);
+        return (1);
+}i
