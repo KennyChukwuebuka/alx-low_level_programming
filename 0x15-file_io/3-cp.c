41a907a5392de265d35361eec33f10cec40a8ac2
kennyChukwuebuka <kenkinado@gmail>
2023-08-07 16:57:08 -0700

    Hotfix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,31 +1,24 @@
 #include "main.h"
 /**
- * main - main entry
- * @argc: arg count
- * @argv: arg vector
- *
+ * copy_file - Function that copy file
+ * @file_from: Params
+ * @file_to: Params
  * Return: 0
  */
-int main(int argc, char *argv[])
+void copy_file(char *file_from, char *file_to)
 {
-	char *file_from = argv[1];
-	char *file_to = argv[2];
 	int fd_from, fd_to;
 	char buffer[BUFFER_SIZE];
 	ssize_t bytes_read, bytes_written;
 
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: %s file_from_to\n", argv[0]);
-			exit(97);
-	}
 	fd_from = open(file_from, O_RDONLY);
 	if (fd_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 		exit(98);
 	}
-	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR |
+			S_IRGRP | S_IWGRP | S_IROTH);
 	if (fd_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
@@ -45,15 +38,28 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 		exit(98);
 	}
-	if (close(fd_from) == -1)
+	if (close(fd_from) == -1 || close(fd_to) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
 		exit(100);
 	}
-	if (close(fd_to) == -1)
+}
+
+/**
+ * main - Entry Point
+ * @argc: arg count
+ * @argv: arg vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
+		dprintf(STDERR_FILENO, "Usage: %s file_from_to\n", argv[0]);
+		exit(97);
 	}
+	copy_file(argv[1], argv[2]);
 	return (0);
 }
+
