a3360c4a3eb41b38334f9c1a766f9177751d0a2d
kennyChukwuebuka <kenkinado@gmail>
2023-08-09 15:25:43 +0100

    Function that prints an ELF file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,9 +8,29 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <string.h>
+#include <stdint.h>
+
+#define ELF_MAGIC_SIZE 4
 
 #define BUFFER_SIZE 1024
 
+typedef struct {
+    unsigned char e_ident[ELF_MAGIC_SIZE];
+    uint16_t e_type;
+    uint16_t e_machine;
+    uint32_t e_version;
+    uint64_t e_entry;
+    uint64_t e_phoff;
+    uint64_t e_shoff;
+    uint32_t e_flags;
+    uint16_t e_ehsize;
+    uint16_t e_phentsize;
+    uint16_t e_phnum;
+    uint16_t e_shentsize;
+    uint16_t e_shnum;
+    uint16_t e_shstrndx;
+} ElfHeaderData;
 
 int append_text_to_file(const char *filename, char *text_content);
 int _putchar(char c);
