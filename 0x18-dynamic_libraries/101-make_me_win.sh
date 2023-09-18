5519758c588060b4f209fa005e798820aadbf2b2
kennyChukwuebuka <kenkinado@gmail>
2023-09-18 22:49:49 +0100

    win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://github.com/KennyChukwuebuka/alx-low_level_programming/raw/master/0x18-dynamic_libraries/101-make_me_win.sh -O /tmp/101-make_me-win.sh
-LD_PRELOAD=/tmp/101-make_me_win.sh
+wget https://github.com/KennyChukwuebuka/alx-low_level_programming/raw/master/0x18-dynamic_libraries/shlib.so -O /tmp/shlib.so
+LD_PRELOAD=/tmp/shlib.so
