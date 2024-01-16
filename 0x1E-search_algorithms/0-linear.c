64a05691b811f4682becc0c88beb71d9cc40bc23
kennyChukwuebuka <kenkinado@gmail>
2024-01-16 22:45:57 +0100

    Linear search algo

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,7 +1,29 @@
 #include "search_algos.h"
 #include <stdio.h>
 
+/**
+ * linear_search - function that perfoms a search with LSA
+ * @array: pointer to the first ele to search
+ * @size: number of ele in an array
+ * @value: value to search
+ * Return: first index where value is alloc
+ */
 int linear_search(int *array, size_t size, int value)
 {
+	size_t i;
 
+	if (array == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+
+		if (array[i] == value)
+		{
+			printf("Found %d at index: %lu\n", value, i);
+			return (i);
+		}
+	}
+	return (-1);
 }
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,6 +1,8 @@
 #ifndef _SEARCH_ALGOS_H_
 #define _SEARCH_ALGOS_H_
 
+#include <stddef.h>
+
 int linear_search(int *array, size_t size, int value);
 
 #endif /*search_algos.h*/
